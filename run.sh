a2368ba7a7e4266262963fab186af9dc19316fe7
Charles Smith <cgsmithjr@users.noreply.github.com>
2023-10-21 20:43:13 -0400

    Modified file with local parameters

--- run.sh
+++ run.sh
@@ -2,21 +2,21 @@
 
 VERSION=18.0.3.450
 USER=farfui
-HOSTNAME=pbx.yourdomain.com
+HOSTNAME=galaxy-srv-3cx.cgsmith.me
 REPO=${USER}/3cx:${VERSION}
 
 docker run \
         -d \
         --name 3cx \
         --hostname ${HOSTNAME} \
-        --memory 2g \
-        --memory-swap 2g \
-        --network host \
+        --memory 1g \
+        --memory-swap 1g \
+        --network IPVLAN5 \
         --restart unless-stopped \
         -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-        -v 3cx_backup:/srv/backup \
-        -v 3cx_recordings:/srv/recordings \
-        -v 3cx_log:/var/log \
+        -v /volume1/docker/3cx/backup:/srv/backup \
+        -v /volume1/docker/3cx/recordings:/srv/recordings \
+        -v /volume1/docker/3cx/log:/var/log \
         --cap-add SYS_ADMIN \
         --cap-add NET_ADMIN \
         ${REPO}
